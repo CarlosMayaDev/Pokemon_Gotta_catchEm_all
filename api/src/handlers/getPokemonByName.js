da095f5f65915b0acf891ba52b1656524d057b9b
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-10-17 18:54:43 -0500

    SearchBar finds pokemons from database or api, back and front modified

--- api/src/handlers/getPokemonByName.js
+++ api/src/handlers/getPokemonByName.js
@@ -1,44 +1,63 @@
 const axios = require('axios');
 const { Pokemon } = require('../db');
-const { Op } = require('sequelize');
 
 const getPokemonByName = async (req, res) => {
-
     const { name } = req.query;
     const URL = 'https://pokeapi.co/api/v2/pokemon';
 
     try {
+        // Buscar en la base de datos
+        const responseDB = await Pokemon.findOne({
+            where: { nombre: name },
+        });
 
-        // const dbPokemonByName = await Pokemon.findOne({ 
-        //     where: {
-        //         nombre: name, 
-        //     }
-        // });
-
-        const response = await axios.get(`${URL}/${name.toLowerCase()}`);
-        const pokemonData = response.data;
-        const apiPokemonByName = {
-            id: pokemonData.id,
-            nombre: pokemonData.name,
-            imagen: pokemonData.sprites.other["official-artwork"].front_default,
-            vida: pokemonData.stats[0].base_stat,
-            ataque: pokemonData.stats[1].base_stat,
-            defensa: pokemonData.stats[2].base_stat,
-            velocidad: pokemonData.stats[5]?.base_stat,
-            altura: pokemonData?.height,
-            peso: pokemonData?.weight,
-            tipo: pokemonData.types.map((tipo) => tipo.type.name),
-            creado: false,
+        let dbPokemonByName = null;
+        if (responseDB) {
+            dbPokemonByName = {
+                id: responseDB.id,
+                nombre: responseDB.nombre,
+                imagen: responseDB.imagen,
+                vida: responseDB.vida,
+                ataque: responseDB.ataque,
+                defensa: responseDB.defensa,
+                velocidad: responseDB.velocidad,
+                altura: responseDB.altura,
+                peso: responseDB.peso,
+                tipo: typeof responseDB.tipo === 'string' ? responseDB.tipo.split(",") : responseDB.tipo,
+                creado: true,
+            };
         }
 
-        //  res.status(200).json({ dbPokemonByName: dbPokemonByName, apiPokemonByName: apiPokemonByName } );
-        res.status(200).json(apiPokemonByName);
+        if (!dbPokemonByName) {
+            // Si no se encuentra en la base de datos, buscar en la API
+            const responseAPI = await axios.get(`${URL}/${name.toLowerCase()}`);
+            const pokemonDataAPI = responseAPI.data;
 
+            const apiPokemonByName = {
+                id: pokemonDataAPI.id,
+                nombre: pokemonDataAPI.name,
+                imagen: pokemonDataAPI.sprites.other["official-artwork"].front_default,
+                vida: pokemonDataAPI.stats[0].base_stat,
+                ataque: pokemonDataAPI.stats[1].base_stat,
+                defensa: pokemonDataAPI.stats[2].base_stat,
+                velocidad: pokemonDataAPI.stats[5]?.base_stat,
+                altura: pokemonDataAPI?.height,
+                peso: pokemonDataAPI?.weight,
+                tipo: pokemonDataAPI.types.map((tipo) => tipo.type.name),
+                creado: false,
+            };
+
+            // Respondemos con un array que contiene ambos resultados
+            res.status(200).json([apiPokemonByName, dbPokemonByName]);
+        } else {
+            // Respondemos solo con el resultado de la base de datos
+            res.status(200).json([dbPokemonByName]);
+        }
     } catch (error) {
-        
         res.status(500).json({ error: error.message });
-
     }
 };
 
 module.exports = getPokemonByName;
+
+
